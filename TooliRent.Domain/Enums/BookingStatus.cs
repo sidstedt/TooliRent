bae1a256b6654bfdb2e131f07213910a01eaae11
sidstedt <daniel.sidstedt@gmail.com>
2025-09-08 12:09:43 +0200

    Created Enums for different statuses that will be used throughout the entites that will be created.

---
+++ TooliRent.Domain/Enums/BookingStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace TooliRent.Domain.Enums
+{
+    public enum BookingStatus
+    {
+        Pending = 0,
+        Confirmed = 1,
+        Cancelled = 2,
+        Completed = 3
+    }
+}
---
+++ TooliRent.Domain/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace TooliRent.Domain.Enums
+{
+    public enum UserRole
+    {
+        Admin = 0,
+        Member = 1
+    }
+}
